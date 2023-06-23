4d0b50bf9cb258d6848ee62710c03da8555015db
AnthonySavory <70455322+AnthonySavory@users.noreply.github.com>
2023-06-22 20:02:00 -0700

    6/22/2023
    Created dao and service files as well as added user register

---
+++ src/main/java/DAO/AccountDAO.java
@@ -0,0 +1,40 @@
+package DAO;
+
+import Model.Account;
+
+import Util.ConnectionUtil;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+import Model.Account;
+
+public class AccountDAO {
+
+    public Account UserRegister(Account account) {
+        Connection connection = ConnectionUtil.getConnection();
+        try {
+            // Inserting into the account database
+            // Username, password
+
+            String sql = "Insert into account (username, password) values (?,?)";
+            PreparedStatement preparedStatement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+
+            // write preparedStatement's setString method here.
+            preparedStatement.setString(1, account.username);
+            preparedStatement.setString(2, account.password);
+
+            preparedStatement.executeUpdate();
+            ResultSet pkeyResultSet = preparedStatement.getGeneratedKeys();
+            if (pkeyResultSet.next()) {
+                int generated_account_id = (int) pkeyResultSet.getLong(1);
+                return new Account(generated_account_id, account.username, account.password);
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return null;
+    }
+
+}
